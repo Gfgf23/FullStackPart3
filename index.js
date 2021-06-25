e46c86114ff735f2ff3a28f771779222077051ec
Ali <alilool440@gmail.com>
2021-06-25 10:06:36 +0300

    Added build to the server

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 morgan.token('data', (request, response) => { return JSON.stringify(request.body) })
 app.use(morgan('tiny'))
