8b0228a3d2c049db40cb0c33085c4b57e1b7f5a9
Ali <alilool440@gmail.com>
2021-06-28 06:45:47 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
+const mongoose = require('mongoose')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./modules/person')
+
 
 const app = express()
 
@@ -12,29 +14,43 @@ app.use(express.json())
 morgan.token('data', (request, response) => { return JSON.stringify(request.body) })
 app.use(morgan('tiny'))
 app.use(morgan(':data'))
-const persons = "hello"
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+	  console.log(error.name)
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 Person.find({}).then(result =>{
 	//persons = result.toJSON()
 	const persons = result
 	console.log(persons)
 })
-console.log(persons)
+
 
 
 const generateId = () => {
 		return Math.floor(Math.random() * 300)
 	}
 	
-const isNameExits = (name) => {
-	const check = persons.map(person => person.name).includes(name)
-	return check 
+const isNameExits = (name,persons) => {
+	return persons.map(person => person.name).includes(name)
+	
+	
 	
 }
 	
 app.get('/info', (request, response) => {
-  response.send(`Phonebook has info for ${persons.length} people \n
-	  ${new Date()}
-  `)
+    Person.find({}).then(persons => {
+		response.send(`Phonebook has info for ${persons.length} people \n
+	  ${new Date()}`)
+	})
 })
 
 app.get('/api/persons', (request, response) => {
@@ -44,29 +60,38 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id',(request,response) => {
-	const id = Number(request.params.id)
-	const person = persons.find(p => p.id === id)
-	if(person)
-	response.json(person)
-	else 
-	response.status(404).end()
+	const id = request.params.id
+	Person.findById(id).then(person =>{
+		if(person)
+			response.json(person)
+		else
+			response.status(404).end()
+	})
+	
 	
 })
 
-app.delete('/api/persons/:id', (request,response) => {
-	const id =Number(request.params.id)
-	persons = persons.filter(person => person.id !== id)
-	response.status(204).end()
+app.delete('/api/persons/:id', (request,response,next) => {
+	const id =request.params.id
+	Person.findByIdAndRemove(id).then(result =>{
+		response.status(204).end()
+	
+		
+	})
+	.catch(error =>{
+		next(error)
+	})
+	
 	
 })
 
 app.post('/api/persons', (request,response) =>{
 	
 	const body = request.body 
-
-	if(!body.name || !body.number || isNameExits(body.name)){
-
-		return response.status(400).json({error: 'name must be unique'})
+	Person.find({}).then(persons =>{
+	if(!body.name || !body.number || isNameExits(body.name,persons)){
+		console.log("Duplicate")
+		return response.status(400).end()
 	}
 	const person = new Person({
 		name: body.name,
@@ -75,7 +100,29 @@ app.post('/api/persons', (request,response) =>{
 	})
 	person.save().then(result =>{
 		response.json(person)
-		mongoose.connection.close()
+		//mongoose.connection.close()
+	})
+	})
+	
+	
+})
+
+app.put('/api/persons/:id', (request,response,next) => {
+	const id =request.params.id
+	
+	const body = request.body 
+	
+	const person = {
+		name:body.name,
+		number:body.number
+	}
+	Person.findByIdAndUpdate(id,person,{new:true}).then(result =>{
+		response.json(result)
+	
+		
+	})
+	.catch(error =>{
+		next(error)
 	})
 	
 	
